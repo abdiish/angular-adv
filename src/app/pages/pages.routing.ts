df74ec6a9a045ba24c6bfce768f46e49ffe02375
Alan Cortes <ing.ti.upmh@gmail.com>
2021-07-26 22:35:58 -0500

    Fin seccion 8

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 const routes: Routes = [
@@ -15,19 +17,33 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            component: DashboardComponent
+            component: DashboardComponent,
+            data: { titulo: 'Dashboard' } 
           },
           {
             path: 'progress',
-            component: ProgressComponent
+            component: ProgressComponent,
+            data: { titulo: 'ProgressBar' }
           },
           {
             path: 'grafica1',
-            component: Grafica1Component
+            component: Grafica1Component,
+            data: { titulo: 'Gráfica' }
           },
           {
             path: 'account-settings',
-            component: AccountSettingsComponent
+            component: AccountSettingsComponent,
+            data: { titulo: 'Ajustes' }
+          },
+          {
+            path: 'promesas',
+            component: PromesasComponent,
+            data: { titulo: 'Promesas' }
+          },
+          {
+            path: 'rxjs',
+            component: RxjsComponent,
+            data: { titulo: 'RxJs' }
           }
         ]
       }
