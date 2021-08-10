580691abde2b2e258807bac47649baa6ce5bda25
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-09 19:03:04 -0500

    Validaciones
    Se realiza la validación del formulario de registro
    
    1.- Se crean dos funciones para validar campos(email,nombre,contraseña).
    
    2.- Se crea una función para validar los terminos de uso
    
    Nota: Para validar algunos campos,se hace uso de Validators de Angular

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -10,12 +10,14 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent  {
 
+  public formSubmitted = false;
+
   public registerForm = this.fb.group({
 
-    nombre: ['Alan', Validators.required],
-    email: ['test100@gmail.com', Validators.required],
-    password: ['123456', Validators.required],
-    password2: ['123456', Validators.required],
+    nombre: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email] ],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
     terminos: [false, Validators.required]
 
   });
@@ -23,8 +25,30 @@ export class RegisterComponent  {
   constructor( private fb: FormBuilder) { }
 
   crearUsuario() {
+
+    this.formSubmitted = true;
      console.log( this.registerForm.value );
+
+     if ( this.registerForm.valid ) {
+        console.log('Posteando formulario');
+     } else {
+       console.log('Formulario no es correcto');
+     }
      
+  }
+
+  campoNoValido( campo: string):boolean {
+
+    if ( this.registerForm.get(campo)?.invalid && this.formSubmitted ) {
+      return  true
+    }else{
+      return false;
+    }
+  }
+
+  aceptaTerminos() {
+
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
 
 }
