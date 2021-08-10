ec06d7242b1179a65a09a5c471e6ecdd70351009
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-10 15:35:27 -0500

    Login de usuario
    1.- Se crea interfaz LoginForm
    
    2.- Se modifica inputs de archivo login.component.html
    
    3.- Se implementa Sweet Alert y se implementa función login en el
      archivo ususario.service.ts
    
    4.- Se reemplaza imagen background login-register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +11,35 @@ import { Router } from '@angular/router';
     './login.component.css'
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor( private router: Router) { }
+  public loginForm = this.fb.group({
 
-  ngOnInit(): void {
-  }
+    email: ['alancr@gmail.com', [Validators.required, Validators.email] ],
+    password: ['123456', Validators.required],
+    remember: [false]
 
-  login(){
-    
-    this.router.navigateByUrl('/');
+  });
+
+  constructor( private router: Router,
+               private fb: FormBuilder,
+               private usuarioService: UsuarioService ) { }
+
+
+
+  login() {
+
+    this.usuarioService.login( this.loginForm.value )
+      .subscribe( resp => {
+        console.log(resp);
+        
+      }, (err) =>{
+        Swal.fire('Error', err.error.msg, 'error');
+      });
+
+    // Recuperar valores capturados en formulario
+    // console.log( this.loginForm.value);
+    // this.router.navigateByUrl('/');
     
   }
 
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { environment } from '../../environments/environment';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url;
 
@@ -16,6 +17,11 @@ export class UsuarioService {
 
     return this.http.post(`${ base_url }/usuarios`, formData);
     
+  }
+
+  login( formData: LoginForm ) {
+
+    return this.http.post(`${ base_url }/login`, formData);
   }
 
 }
