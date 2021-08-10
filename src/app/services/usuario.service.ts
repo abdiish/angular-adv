f5915e67cb2f6237f993dad0ef2435c682071bdc
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-10 16:03:04 -0500

    Local Storage
    1.- Se guarda el token de inicio de sesión en Local Storage
    
    2.- Se hace uso de pipe en usuario.service.ts

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { RegisterForm } from '../interfaces/register-form.interface';
+import { tap } from 'rxjs/operators';
+
 import { environment } from '../../environments/environment';
+
+import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url;
@@ -15,13 +18,23 @@ export class UsuarioService {
 
   crearUsuario( formData: RegisterForm ) {
 
-    return this.http.post(`${ base_url }/usuarios`, formData);
+    return this.http.post(`${base_url}/usuarios`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token)
+        })
+      );
     
   }
 
   login( formData: LoginForm ) {
 
-    return this.http.post(`${ base_url }/login`, formData);
+    return this.http.post(`${base_url}/login`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token)
+        })
+      );
   }
 
 }
